bc72921d653d155d869f5eb4f2ffd79ba5402000
FellipeCavalcante <azevedoaranha1@gmail.com>
2024-06-11 22:36:14 -0300

    loops

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+  public static void main(String[] args) {
+    for (int carneirinhos = 0; carneirinhos <= 20; carneirinhos++) {
+      System.out.println("Carneirinho número " + carneirinhos);
+    }
+    System.out.println("Rei delas dormiu");
+  }
+}
